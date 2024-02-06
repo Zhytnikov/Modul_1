f401c9b6d245627a35b796e553725c1f1817b44c
Zhytnikov <sashazhytnikov@gmail.com>
2024-02-06 21:24:38 +0200

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,16 @@
+from Bot import AddBot, ExitBot, SearchBot
+
+if __name__ == "__main__":
+    choice = {
+        'add': AddBot(),
+        'search': SearchBot(),
+        'exit': ExitBot(),
+    }
+
+    action = input("Choose an action: ")
+
+    while True:
+        if action in choice:
+            choice[action].handle()
+        else:
+            print("Incorrect action!")
